c5b58a7149b76aa893efe018e5f427194034936f
Grotius Hasiholan <grotius.hasiholan@gmail.com>
2021-10-19 22:37:31 +0700

    Rebuilding the game :D

--- js/game.js
+++ js/game.js
@@ -1,110 +1,172 @@
-var pRock = document.getElementById('r-player')
-var pPaper = document.getElementById('p-player')
-var pScissors = document.getElementById('s-player')
+var refresh = document.getElementById('reset')
+var versus = document.getElementById('versus')
+var matchResult = document.getElementById('result')
 
-var cRock = document.getElementById('r-com')
-var cPaper = document.getElementById('p-com')
-var cScissors = document.getElementById('s-com')
-
-var matchResult = document.getElementById('result-p')
-var matchResultBox = document.getElementById('result-div')
-
-var refreshText = document.getElementById('refresh-text')
-
-function pilihanComp() {
-    const pil = Math.floor(Math.random() * 3);
-    if (pil == 0) {
-        cRock.style.backgroundColor = "#C4C4C4";
-        return "rock";
-    } else if (pil == 1) {
-        cPaper.style.backgroundColor = "#C4C4C4";
-        return "paper";
-    } else { // pil==2
-        cScissors.style.backgroundColor = "#C4C4C4";
-        return "scissors";
-    }
+function notEmpty() {
+    refresh.children[0].style.width = '8em'
+    console.log('Refresh button was clicked')
 }
 
-function playerVsComp(player, computer) {
-    if (player == computer) {
-        return 'draw'
-    } else if (player == 'rock') {
-        if (computer == 'paper') {
-            return 'lose'
-        } else {
-            return 'win'
+class Participant {
+    constructor() {
+        this.name = this.constructor.name
+        this.selection = undefined
+        this.tool = undefined
+    }
+
+    selected() {
+        var tool = document.getElementById(this.selection)
+        tool.style.backgroundColor = '#C4C4C4'
+        tool.style.borderRadius = '25px'
+    }
+
+    reset() {
+        var tool = document.getElementById(this.selection)
+        refresh.children[0].style.width = '5em'
+        if (tool) {
+            tool.style.backgroundColor = ''
+            tool.style.borderRadius = ''
         }
-    } else if (player == 'paper') {
-        if (computer == 'scissors') {
-            return 'lose'
+        this.selection = undefined
+        this.tool = undefined
+    }
+}
+
+class Player extends Participant {
+    constructor() {
+        super()
+    }
+
+    chooseRock() {
+        if (!this.selection) {
+            this.selection = 'r-player'
+            this.tool = 'rock'
+            this.selected()
+            config()
         } else {
-            return 'win'
+            notEmpty()
+            console.log('Object is not empty')
         }
-    } else {
-        if (computer == 'rock') {
-            return 'lose'
+    }
+
+    choosePaper() {
+        if (!this.selection) {
+            this.selection = 'p-player'
+            this.tool = 'paper'
+            this.selected()
+            config()
         } else {
-            return 'win'
+            notEmpty()
+            console.log('Object is not empty')
+        }
+    }
+
+    chooseScissor() {
+        if (!this.selection) {
+            this.selection = 's-player'
+            this.tool = 'scissor'
+            this.selected()
+            config()
+        } else {
+            notEmpty()
+            console.log('Object is not empty')
         }
     }
 }
 
-function reset() {
-    matchResultBox.style.visibility = "hidden";
-    pRock.style.removeProperty("background-color");
-    pPaper.style.removeProperty("background-color");
-    pScissors.style.removeProperty("background-color");
-    cRock.style.removeProperty("background-color");
-    cPaper.style.removeProperty("background-color");
-    cScissors.style.removeProperty("background-color");
-}
+class Computer extends Participant {
+    constructor() {
+        super()
+    }
 
-function showHasil(result) {
-    matchResultBox.style.removeProperty("visibility")
-    if (result == "draw") {
-        matchResult.innerText = "DRAW";
-    } else if (result == "lose") {
-        matchResult.innerText = "COM\nWIN";
-    } else {
-        matchResult.innerText = "PLAYER 1\nWIN";
+    randomToolSelection() {
+        const pil = Math.floor(Math.random() * 3);
+        switch (pil) {
+            case 0:
+                this.tool = 'rock'
+                this.selection = 'r-com'
+                break
+            case 1:
+                this.tool = 'paper'
+                this.selection = 'p-com'
+                break
+            case 2:
+                this.tool = 'scissor'
+                this.selection = 's-com'
+                break
+        }
     }
 }
 
-function chooseRock() {
-    if (pRock.style.backgroundColor == '#C4C4C4' || pPaper.style.backgroundColor == '#C4C4C4' || pScissors.style.backgroundColor == '#C4C4C4') {
-        refreshText.removeProperty('visibility')
-    } else {
-        reset()
-        pRock.style.backgroundColor = "#C4C4C4"
-        let player = "rock"
-        let computer = pilihanComp()
-        let result = playerVsComp(player, computer)
-        showHasil(result)
+// var player = new Player()
+
+var player = new Player()
+var computer = new Computer()
+
+function result(){
+    var a = player.tool
+    var b = computer.tool
+    //choosing the winner
+    if(a === 'rock'){
+        if (b === 'rock'){
+            var hasil = 'draw'
+        } else if (b === 'paper'){
+            var hasil = 'lose'
+        } else {
+            var hasil = 'win'
+        }
+    } else if(a === 'paper'){
+        if (b === 'rock'){
+            var hasil = 'win'
+        } else if (b === 'paper'){
+            var hasil = 'draw'
+        } else {
+            var hasil = 'lose'
+        }
+    } else{
+        if (b === 'rock'){
+            var hasil = 'lose'
+        } else if (b === 'paper'){
+            var hasil = 'win'
+        } else {
+            var hasil = 'draw'
+        }
     }
+    console.log(hasil)
+    showingResult(hasil)
 }
 
-function choosePaper() {
-    if (pRock.style.backgroundColor == '#C4C4C4' || pPaper.style.backgroundColor == '#C4C4C4' || pScissors.style.backgroundColor == '#C4C4C4') {
-        refreshText.removeProperty('visibility')
+function showingResult(hasil){
+    if(hasil== 'win'){
+        matchResult.children[0].innerHTML = 'PLAYER <br> WIN'
+        matchResult.style.backgroundColor = '#4C9654'
+        matchResult.style.paddingTop = ''
+        matchResult.style.visibility = 'visible'
+    } else if(hasil == 'lose'){
+        matchResult.children[0].innerHTML = 'COMPUTER <br> WIN'
+        matchResult.style.backgroundColor = '#4C9654'
+        matchResult.style.paddingTop = ''
+        matchResult.style.visibility = 'visible'
     } else {
-        reset()
-        pPaper.style.backgroundColor = "#C4C4C4"
-        let player = "paper"
-        let computer = pilihanComp()
-        let result = playerVsComp(player, computer)
-        showHasil(result)
+        matchResult.children[0].innerHTML = 'DRAW'
+        matchResult.style.backgroundColor = '#035B0C'
+        matchResult.style.paddingTop = '1.5em'
+        matchResult.style.visibility = 'visible'
     }
+    versus.style.display = 'none'
 }
 
-function chooseScissor() {
-    if (pRock.style.backgroundColor == '#C4C4C4' || pPaper.style.backgroundColor == '#C4C4C4' || pScissors.style.backgroundColor == '#C4C4C4') {
-        refreshText.removeProperty('visibility')
-    } else {
-        reset()
-        pScissors.style.backgroundColor = "#C4C4C4"
-        let player = "scissors"
-        let computer = pilihanComp()
-        let result = playerVsComp(player, computer)
-        showHasil(result)
-    }
+function config(){
+    console.log("Computer's turn")
+    computer.randomToolSelection()
+    computer.selected()
+    result()
+    
+}
+
+function reset(){
+    player.reset()
+    computer.reset()
+    versus.style.display = ''
+    matchResult.style.visibility = 'hidden'
 }
